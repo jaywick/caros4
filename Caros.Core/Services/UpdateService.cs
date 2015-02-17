1c0dd50651ed5300ddd2f06e0d45f45697af4ac0
Jay Wick <developer@jay-wick.com>
2015-02-17 20:37:18 +1100

    #13: Constants for new FTP folder

---
+++ Caros.Core/Services/UpdateService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caros.Core.Services
+{
+    class UpdateService : Service
+    {
+        private const string FtpHost = "103.9.171.165";
+        private const string FtpPath = @"/caros4/packages";
+        private const string FtpUserName = "caros@jay-wick.com";
+        private const string FtpPassword = "_=nMqH!m@naV";
+
+        public override void Start()
+        {
+        }
+    }
+}
