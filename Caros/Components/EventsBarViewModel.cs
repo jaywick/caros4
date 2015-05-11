7a20d4a1bb1649ce5425a576d4925d2068734655
Jay Wick <developer@jay-wick.com>
2015-05-11 20:40:03 +1000

    Hide events after timeout of 5 seconds

--- Caros/Components/EventsBarViewModel.cs
+++ Caros/Components/EventsBarViewModel.cs
@@ -8,6 +8,7 @@ using Caros.Core.Context;
 using Caros.Core.UI;
 using Caros.Core.Services;
 using Caros.Core;
+using System.Windows.Threading;
 
 namespace Caros.Components
 {
@@ -18,11 +19,28 @@ namespace Caros.Components
         public BindableCollection<NamedAction> Tasks { get; set; }
         public EventPost Post { get; private set; }
 
+        private DispatcherTimer _timer;
+
         public EventsBarViewModel(EventPost post)
         {
             Post = post;
+
+            CreateTasks(post);
+            StartTimer();
+        }
+
+        private void CreateTasks(EventPost post)
+        {
             Tasks = new BindableCollection<NamedAction>();
             Tasks.AddRange(post.Tasks);
+        }
+
+        private void StartTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(5);
+            _timer.Tick += (s, e) => Close();
+            _timer.Start();
         }
 
         public void Close()
